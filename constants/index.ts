14fc69bb7f505aff89939ba69c187dbb642aa906
dbahiense <douglas@rohling.com.br>
2024-08-02 11:57:34 -0300

    add constants file

---
+++ constants/index.ts
@@ -0,0 +1,34 @@
+export const escolaridades = [
+  {i: 0, content: 'Analfabeta'},
+  {i: 1, content: 'Fundamental Incompleto'},
+  {i: 2, content: 'Fundamental Completo'},
+  {i: 3, content: 'Médio Incompleto'},
+  {i: 4, content: 'Médio Completo'},
+  {i: 5, content: 'Superior Incompleto'},
+  {i: 6, content: 'Superior Completo'},
+  {i: 7, content: 'Mestrado'},
+  {i: 8, content: 'Doutorado'},
+]
+
+export const series = [
+  {i: 0, v: 'Pré-escola'},
+  {i: 1, v: '1º ano'},
+  {i: 2, v: '2º ano'},
+  {i: 3, v: '3º ano'},
+  {i: 4, v: '4º ano'},
+  {i: 5, v: '5º ano'},
+  {i: 6, v: '6º ano'},
+  {i: 7, v: '7º ano'},
+  {i: 8, v: '8º ano'},
+  {i: 9, v: '9º ano'},
+  {i: 11, v: '1ᵃ série'},
+  {i: 12, v: '2ᵃ série'},
+  {i: 13, v: '3ᵃ série'},
+  {i: 14, v: 'Superior'},
+  {i: 15, v: 'Não estuda'},
+]
+
+export const genero = [
+  {i:0, v: 'feminino'},
+  {i:1, v: 'masculino'},
+]
