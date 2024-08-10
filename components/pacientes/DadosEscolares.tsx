51bc71a9419c26ff8c07ee53b233573aaf9c73d5
dbahiense <douglas@rohling.com.br>
2024-08-10 12:30:14 -0300

    dados escolares

--- components/pacientes/DadosEscolares.tsx
+++ components/pacientes/DadosEscolares.tsx
@@ -1,96 +1,96 @@
-import React from 'react';
-import 'dayjs/locale/pt-br';
-import { AdapterDayjs } from  '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from    '@mui/x-date-pickers/DatePicker';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import React, { useState } from 'react';
+import { Button, Checkbox, Divider, FormControlLabel, TextField, Typography } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
-import { Autocomplete, Divider, FormControlLabel, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, TextareaAutosize } from '@mui/material';
-import FormControl from '@mui/material/FormControl';
-import Switch from '@mui/material/Switch';
 
-export default function Date() {
+const DadosEscolares = () => {
+  const [isReforcoEscolarChecked, setIsReforcoEscolarChecked] = useState(false);
+  const [isExtraCurricularChecked, setIsExtracurricularChecked] = useState(false);
 
-  const [age, setAge] = React.useState('');
+  const handleReforcoEscolarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsReforcoEscolarChecked(event.target.checked);
+  };
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value as string);
-  };  
-
+  const handleExtracurricularChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsExtracurricularChecked(event.target.checked);
+  };
 
   return (
     <>
-    <Grid container spacing={2}>
+      <Grid container spacing={2}>
+        <Grid xs={12} md={8}>
+          <TextField id='escola' label="Nome da Escola" fullWidth variant="outlined" />
+        </Grid>
 
-      <Grid xs={12} md={4}>
-        <TextField id='escola' label="Nome da escola" fullWidth variant="outlined" />
-      </Grid>
+        <Grid xs={12} md={2}>
+          <TextField id='turno' label="Turno" fullWidth variant="outlined" />
+        </Grid>
 
-      <Grid xs={12} md={4}>
-        <TextField id='serie' label="Ano / Série" fullWidth variant="outlined" />
-      </Grid>
+        <Grid xs={12} md={2}>
+          <TextField id='serie' label="Ano / Série" fullWidth variant="outlined" />
+        </Grid>
 
-      <Grid xs={12} md={4}>
-        <FormControl fullWidth>
-          <InputLabel id="turno">Turno</InputLabel>
-          <Select
-            id="turno"
-            value={age}
-            label="Turno"
-            onChange={handleChange}
-          >
-            <MenuItem value={0}>Manhã</MenuItem>
-            <MenuItem value={1}>Tarde</MenuItem>
-            <MenuItem value={2}>Noite</MenuItem>
-            <MenuItem value={3}>Integral</MenuItem>
-          </Select>
-        </FormControl>
-      </Grid>
+        <Grid xs={12} md={6}>
+          <TextField id='coordenadora' label="Nome da Coordenadora Pedagógica" fullWidth variant="outlined" />
+        </Grid>
 
-      <Grid xs={12} md={4}>
-        <TextField id='coordenadora' label="Nome da coordenadora" fullWidth variant="outlined" />
-      </Grid>
+        <Grid xs={12} md={6}>
+          <TextField id='professora' label="Nome da Professora" fullWidth variant="outlined" />
+        </Grid>
 
-      <Grid xs={12} md={4}>
-        <TextField id='professora' label="Nome da professora" fullWidth variant="outlined" />
-      </Grid>
-
-      <Grid xs={12} md={6}>
-        <FormControlLabel
-          value="start"
-          control={<Switch color="primary" />}
-          label="Reforço escolar?"
-          labelPlacement="start"
-        />
-        <TextField
-          label="Matérias"
-          multiline
-          rows={4}
-        >
-        </TextField>
-      </Grid>
-
-      <Grid xs={12} md={6}>
-        <FormControlLabel
-            value="start"
-            control={<Switch color="primary" />}
-            label="Atividades extra curriculares?"
-            labelPlacement="start"
+        <Grid xs={12} md={6}>
+          &nbsp;<FormControlLabel
+            control={<Checkbox checked={isReforcoEscolarChecked} onChange={handleReforcoEscolarChange} />}
+            label="Reforço Escolar?"
           />
+          {isReforcoEscolarChecked && (
+            <TextField
+              id="materias"
+              label="Matérias"
+              fullWidth
+              multiline
+              rows={3}
+            />
+          )}
+        </Grid>
+
+        <Grid xs={12} md={6}>
+          &nbsp;<FormControlLabel
+            control={<Checkbox checked={isExtraCurricularChecked} onChange={handleExtracurricularChange} />}
+            label="Atividades extra-curriculares?"
+          />
+          {isExtraCurricularChecked && (
+            <TextField
+              id="atividades"
+              label="Atividades"
+              fullWidth
+              multiline
+              rows={3}
+            />
+          )}
+        </Grid>
       </Grid>
 
 
+      <Divider sx={{my: '2rem ',}}/>
 
 
-    </Grid>
+      <Grid container spacing={2}>
+        <Grid xs={6} md={6}>
+          <Button variant="contained">Guardar Dados</Button>
+        </Grid>
+
+        <Grid xs={3} md={3}>
+          <Typography variant='caption'>Criado por:</Typography>
+          <Typography variant='caption' display={'block'}>Carina em 10/08/24</Typography>
+        </Grid>
+
+        <Grid xs={3} md={3}>
+          <Typography variant='caption'>Modificado por:</Typography>
+          <Typography variant='caption' display={'block'}>Rafael em 18/08/24</Typography>
+        </Grid>
+      </Grid>
     </>
   );
 }
 
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const top100Films = [
-  { label: 'Espírito Santo', year: 1994 },
-  { label: 'Minas Gerais', year: 1972 },
-  { label: 'Rio de Janeiro', year: 1974 },
-  { label: 'São Paulo', year: 2008 },
-];
+export default DadosEscolares;
