a09a955e5f49715e13a80ad361dec8f741101e8e
dbahiense <douglas@rohling.com.br>
2024-08-31 11:51:40 -0300

    feat: add select

--- components/pacientes/Time.tsx
+++ components/pacientes/Time.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import * as React from 'react';
+import { Autocomplete, Button, TextField } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2/Grid2';
 
-const Time = () => {
+const top100Films = [
+  { label: 'Carina', year: 1994 },
+  { label: 'Rafael', year: 1972 },
+  { label: 'Gabriel', year: 1974 },
+  { label: 'Daniel', year: 2008 },
+  { label: 'Douglas', year: 1957 },
+  { label: "Daniela", year: 1993 },
+  { label: 'Lucas', year: 1994 },
+  { label: 'Pedro', year: 1994 },
+  { label: 'João', year: 1994 },
+  { label: 'José', year: 1994 },
+]
+
+export default function ComboBox() {
   return (
-    <div>Time</div>
-  )
+    <Grid container spacing={2}>
+      <Grid xs={6} md={6}>
+        <Autocomplete
+          disablePortal
+          options={top100Films}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Profissional" />}
+        />
+      </Grid>
+      <Grid xs={6} md={6}>
+        <Button variant="contained">Adicionar ao Time</Button>
+      </Grid>
+    </Grid>
+  );
 }
-
-export default Time
